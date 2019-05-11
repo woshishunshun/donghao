ac42d265c0befd4f90402e10e24afef8b19d1f28
顺 齐 <2490475248@qq.com>
2019-05-11 11:35:51 +0800

    添加项目文件。

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1111111
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
